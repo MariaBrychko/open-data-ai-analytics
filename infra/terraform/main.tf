115f638c70a92b0f71f404893b6af072e9902374
Mariia Brychko <mariia.brychko.shi.2023@lpnu.ua>
2026-04-30 15:11:31 +0300

    Edit Azure Terraform deployment

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   computer_name       = "${var.project_name}-vm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B2s"
+  size                = "Standard_B1ms"
   admin_username      = var.admin_username
 
   network_interface_ids = [
